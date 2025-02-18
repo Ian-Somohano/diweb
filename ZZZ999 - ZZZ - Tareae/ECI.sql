4740bf0511abd50a1813c09c0fa2c1681a42042f
Ian <ian.somohano.montoro@gmail.com>
2025-02-18 20:15:25 +0100

    Actualizacion

---
+++ ZZZ999 - ZZZ - Tareae/ECI.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS ECI;
+
+CREATE DATABASE IF NOT EXISTS ECI
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_spanish2_ci;
+
+USE ECI;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    Correo VARCHAR(50) NOT NULL,
+    Clave VARCHAR(50) NOT NULL,
+    Nombre VARCHAR(30) NOT NULL,
+    Autonomo BOOLEAN NOT NULL,
+    Nif_cif INT NOT NULL,
+    PRIMARY KEY pk_usuarios(Correo),
+    INDEX idx_usuarios(Nombre)
+) ENGINE = innodb 
+COMMENT = "Tabla clubes: ENGINE Motor BBDD";
